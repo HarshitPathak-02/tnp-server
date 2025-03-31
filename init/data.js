17e14b7a0aa6b8c4c5713e1f67687dabbe5a0e8c
Harshit Pathak <harshitpathak.hp567@gmail.com>
2025-03-31 14:00:24 +0530

    test data updated

--- init/data.js
+++ init/data.js
@@ -75,191 +75,199 @@ tcsAptitudeQuestions = [
     test_name: "tcs-aptitude",
   },
   {
-    question:
-      "If the price of a shirt is increased by 20%, by how much percent should its consumption be reduced so that the expenditure remains the same?",
-    options: ["16.67%", "14.32%", "16.50%", "12.84%"],
-    correct_option: "16.67%",
+    question: "Do you want to submit the test?",
+    options: ["Yes", "No"],
+    correct_option: "Yes/No",
     company: "tcs",
     test_name: "tcs-aptitude",
-  },
-  {
-    question:
-      "The ratio of boys to girls in a class is 3:5. If there are 36 girls, how many boys are there in the class?",
-    options: ["20", "15", "22", "27"],
-    correct_option: "22",
-    company: "tcs",
-    test_name: "tcs-aptitude",
-  },
-  {
-    question:
-      "A shopkeeper sells an article at a profit of 20%. If the cost price is Rs.250, what is the selling price?",
-    options: ["500", "300", "50", "450"],
-    correct_option: "300",
-    company: "tcs",
-    test_name: "tcs-aptitude",
-  },
-  {
-    question:
-      "A bag contains 4 red balls and 6 blue balls. Two balls are drawn at random. What is the probability that both balls are red?",
-    options: ["1/3", "4/5", "2/15", "6/21"],
-    correct_option: "2/15",
-    company: "tcs",
-    test_name: "tcs-aptitude",
-  },
-  {
-    question:
-      "If 12 workers can build a wall in 9 days, how many days will it take for 8 workers to build the same wall?",
-    options: ["12 days", "10 days", "8 days", "13.5 days"],
-    correct_option: "13.5 days",
-    company: "tcs",
-    test_name: "tcs-aptitude",
-  },
-  {
-    question:
-      "An Informal Gathering occurs when a group of people gets together in a casual, relaxed manner. Which situation below is the best example of an Informal Gathering?",
-    options: [
-      "A debating club meets on the first Sunday morning of every month.",
-      "After finding out about his salary raise, Jay and a few colleagues go out for a quick dinner after work.",
-      "Meena sends out 10 invitations for a bachelorette party she is giving to her elder sister.",
-      "Whenever she eats at a Chinese restaurant, Roop seems to run into Dibya.",
-    ],
-    correct_option:
-      "After finding out about his salary raise, Jay and a few colleagues go out for a quick dinner after work.",
-    company: "tcs",
-    test_name: "tcs-aptitude",
-  },
-  {
-    question: "What is the definition of the term ‘Hypotenuse’?",
-    options: [
-      "The longest side of a right-angled triangle.",
-      "A triangle with three equal sides.",
-      "The angle opposite the longest side of a triangle.",
-      "The sum of the interior angles of a triangle.",
-    ],
-    correct_option: "The longest side of a right-angled triangle.",
-    company: "tcs",
-    test_name: "tcs-aptitude",
-  },
-  {
-    question:
-      "A Tiebreaker is an additional contest carried out to establish a winner among tied contestants. Choose one situation from the options below that best represents a Tiebreaker.",
-    options: [
-      "At halftime, the score is tied at 2-2 in a football match.",
-      "Serena and Maria have each secured 1 set in the game.",
-      "The umpire tosses a coin to decide which team will have to bat first.",
-      "RCB and KKR each finished at 140 all out.",
-    ],
-    correct_option: "RCB and KKR each finished at 140 all out.",
-    company: "tcs",
-    test_name: "tcs-aptitude",
-  },
-  {
-    question: "Match the term ‘Photosynthesis’ with its definition:",
-    options: [
-      "The process of converting light energy into chemical energy.",
-      "The breakdown of glucose to release energy in the absence of oxygen.",
-      "The transport of water and nutrients through plant tissues.",
-      "The production of gametes in organisms for sexual reproduction.",
-    ],
-    correct_option:
-      "The process of converting light energy into chemical energy.",
-    company: "tcs",
-    test_name: "tcs-aptitude",
-  },
-  {
-    question: "Match the term ‘Exponential growth’ with its definition:",
-    options: [
-      "The steady growth of a population over time.",
-      "The growth pattern is characterized by a decreasing rate of growth.",
-      "The rapid growth pattern where the population size increases exponentially.",
-      "The decline in population size due to limited resources.",
-    ],
-    correct_option:
-      "The rapid growth pattern where the population size increases exponentially.",
-    company: "tcs",
-    test_name: "tcs-aptitude",
-  },
-  {
-    question:
-      "Which of the following is the most likely conclusion to be drawn from the following statements?",
-    statements: ["All cats are mammals", "Some mammals are furry"],
-    options: [
-      "All mammals are furry",
-      "Some cats are furry",
-      "Some furry animals are cats",
-      "All furry animals are mammals",
-      "None of the above",
-    ],
-    correct_option: "Some furry animals are cats",
-    company: "tcs",
-    test_name: "tcs-reasoning",
-  },
-  {
-    question:
-      "Which of the following is the most logical inference from the following statements?",
-    statements: [
-      "If it is raining, then the ground is wet",
-      "The ground is wet",
-    ],
-    options: [
-      "It is raining",
-      "It is not raining",
-      "The ground is not wet",
-      "Both (a) and (b) are possible",
-      "None of the above",
-    ],
-    correct_option: "It is raining",
-    company: "tcs",
-    test_name: "tcs-reasoning",
-  },
-  {
-    question: "Which of the following is the most logical sequence of events?",
-    events: [
-      "The sun rises",
-      "The birds start singing",
-      "People wake up",
-      "The flowers open",
-    ],
-    options: [
-      "1, 2, 3, 4",
-      "2, 1, 3, 4",
-      "3, 1, 2, 4",
-      "4, 1, 2, 3",
-      "None of the above",
-    ],
-    correct_option: "1, 2, 3, 4",
-    company: "tcs",
-    test_name: "tcs-reasoning",
-  },
-  {
-    question:
-      "Which of the following is the most accurate analogy to ‘A dog is to a puppy as a cat is to’:",
-    options: [
-      "A kitten",
-      "A mouse",
-      "A lion",
-      "A cheetah",
-      "None of the above",
-    ],
-    correct_option: "A kitten",
-    company: "tcs",
-    test_name: "tcs-reasoning",
-  },
-  {
-    question:
-      "Which of the following is the most logical definition of the word mammal?",
-    options: [
-      "A warm-blooded animal that gives birth to live young",
-      "A cold-blooded animal that lays eggs",
-      "An animal that has fur or hair",
-      "An animal that has four legs",
-      "None of the above",
-    ],
-    correct_option: "A warm-blooded animal that gives birth to live young",
-    company: "tcs",
-    test_name: "tcs-reasoning",
-    
   },
 ];
+//   {
+//     question:
+//       "If the price of a shirt is increased by 20%, by how much percent should its consumption be reduced so that the expenditure remains the same?",
+//     options: ["16.67%", "14.32%", "16.50%", "12.84%"],
+//     correct_option: "16.67%",
+//     company: "tcs",
+//     test_name: "tcs-aptitude",
+//   },
+//   {
+//     question:
+//       "The ratio of boys to girls in a class is 3:5. If there are 36 girls, how many boys are there in the class?",
+//     options: ["20", "15", "22", "27"],
+//     correct_option: "22",
+//     company: "tcs",
+//     test_name: "tcs-aptitude",
+//   },
+//   {
+//     question:
+//       "A shopkeeper sells an article at a profit of 20%. If the cost price is Rs.250, what is the selling price?",
+//     options: ["500", "300", "50", "450"],
+//     correct_option: "300",
+//     company: "tcs",
+//     test_name: "tcs-aptitude",
+//   },
+//   {
+//     question:
+//       "A bag contains 4 red balls and 6 blue balls. Two balls are drawn at random. What is the probability that both balls are red?",
+//     options: ["1/3", "4/5", "2/15", "6/21"],
+//     correct_option: "2/15",
+//     company: "tcs",
+//     test_name: "tcs-aptitude",
+//   },
+//   {
+//     question:
+//       "If 12 workers can build a wall in 9 days, how many days will it take for 8 workers to build the same wall?",
+//     options: ["12 days", "10 days", "8 days", "13.5 days"],
+//     correct_option: "13.5 days",
+//     company: "tcs",
+//     test_name: "tcs-aptitude",
+//   },
+//   {
+//     question:
+//       "An Informal Gathering occurs when a group of people gets together in a casual, relaxed manner. Which situation below is the best example of an Informal Gathering?",
+//     options: [
+//       "A debating club meets on the first Sunday morning of every month.",
+//       "After finding out about his salary raise, Jay and a few colleagues go out for a quick dinner after work.",
+//       "Meena sends out 10 invitations for a bachelorette party she is giving to her elder sister.",
+//       "Whenever she eats at a Chinese restaurant, Roop seems to run into Dibya.",
+//     ],
+//     correct_option:
+//       "After finding out about his salary raise, Jay and a few colleagues go out for a quick dinner after work.",
+//     company: "tcs",
+//     test_name: "tcs-aptitude",
+//   },
+//   {
+//     question: "What is the definition of the term ‘Hypotenuse’?",
+//     options: [
+//       "The longest side of a right-angled triangle.",
+//       "A triangle with three equal sides.",
+//       "The angle opposite the longest side of a triangle.",
+//       "The sum of the interior angles of a triangle.",
+//     ],
+//     correct_option: "The longest side of a right-angled triangle.",
+//     company: "tcs",
+//     test_name: "tcs-aptitude",
+//   },
+//   {
+//     question:
+//       "A Tiebreaker is an additional contest carried out to establish a winner among tied contestants. Choose one situation from the options below that best represents a Tiebreaker.",
+//     options: [
+//       "At halftime, the score is tied at 2-2 in a football match.",
+//       "Serena and Maria have each secured 1 set in the game.",
+//       "The umpire tosses a coin to decide which team will have to bat first.",
+//       "RCB and KKR each finished at 140 all out.",
+//     ],
+//     correct_option: "RCB and KKR each finished at 140 all out.",
+//     company: "tcs",
+//     test_name: "tcs-aptitude",
+//   },
+//   {
+//     question: "Match the term ‘Photosynthesis’ with its definition:",
+//     options: [
+//       "The process of converting light energy into chemical energy.",
+//       "The breakdown of glucose to release energy in the absence of oxygen.",
+//       "The transport of water and nutrients through plant tissues.",
+//       "The production of gametes in organisms for sexual reproduction.",
+//     ],
+//     correct_option:
+//       "The process of converting light energy into chemical energy.",
+//     company: "tcs",
+//     test_name: "tcs-aptitude",
+//   },
+//   {
+//     question: "Match the term ‘Exponential growth’ with its definition:",
+//     options: [
+//       "The steady growth of a population over time.",
+//       "The growth pattern is characterized by a decreasing rate of growth.",
+//       "The rapid growth pattern where the population size increases exponentially.",
+//       "The decline in population size due to limited resources.",
+//     ],
+//     correct_option:
+//       "The rapid growth pattern where the population size increases exponentially.",
+//     company: "tcs",
+//     test_name: "tcs-aptitude",
+//   },
+//   {
+//     question:
+//       "Which of the following is the most likely conclusion to be drawn from the following statements?",
+//     statements: ["All cats are mammals", "Some mammals are furry"],
+//     options: [
+//       "All mammals are furry",
+//       "Some cats are furry",
+//       "Some furry animals are cats",
+//       "All furry animals are mammals",
+//       "None of the above",
+//     ],
+//     correct_option: "Some furry animals are cats",
+//     company: "tcs",
+//     test_name: "tcs-reasoning",
+//   },
+//   {
+//     question:
+//       "Which of the following is the most logical inference from the following statements?",
+//     statements: [
+//       "If it is raining, then the ground is wet",
+//       "The ground is wet",
+//     ],
+//     options: [
+//       "It is raining",
+//       "It is not raining",
+//       "The ground is not wet",
+//       "Both (a) and (b) are possible",
+//       "None of the above",
+//     ],
+//     correct_option: "It is raining",
+//     company: "tcs",
+//     test_name: "tcs-reasoning",
+//   },
+//   {
+//     question: "Which of the following is the most logical sequence of events?",
+//     events: [
+//       "The sun rises",
+//       "The birds start singing",
+//       "People wake up",
+//       "The flowers open",
+//     ],
+//     options: [
+//       "1, 2, 3, 4",
+//       "2, 1, 3, 4",
+//       "3, 1, 2, 4",
+//       "4, 1, 2, 3",
+//       "None of the above",
+//     ],
+//     correct_option: "1, 2, 3, 4",
+//     company: "tcs",
+//     test_name: "tcs-reasoning",
+//   },
+//   {
+//     question:
+//       "Which of the following is the most accurate analogy to ‘A dog is to a puppy as a cat is to’:",
+//     options: [
+//       "A kitten",
+//       "A mouse",
+//       "A lion",
+//       "A cheetah",
+//       "None of the above",
+//     ],
+//     correct_option: "A kitten",
+//     company: "tcs",
+//     test_name: "tcs-reasoning",
+//   },
+//   {
+//     question:
+//       "Which of the following is the most logical definition of the word mammal?",
+//     options: [
+//       "A warm-blooded animal that gives birth to live young",
+//       "A cold-blooded animal that lays eggs",
+//       "An animal that has fur or hair",
+//       "An animal that has four legs",
+//       "None of the above",
+//     ],
+//     correct_option: "A warm-blooded animal that gives birth to live young",
+//     company: "tcs",
+//     test_name: "tcs-reasoning",
+
+//   },
+// ];
 
 module.exports = { tcsAptitudeQuestions: tcsAptitudeQuestions };
